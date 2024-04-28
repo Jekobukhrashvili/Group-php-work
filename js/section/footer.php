5ba6fa65a85e1e61858e103d9ad49b17790643da
Jekobukhrashvili <bukhrashvilijeko@gmail.com>
2024-04-28 23:17:58 +0400

    adding bugfixes

--- js/section/footer.php
+++ js/section/footer.php
@@ -1,25 +1,175 @@
-<footer class="footer_section">
-<div class="container">
-    <p>
-    &copy; <span id="displayYear"></span> All Rights Reserved By
-    <a href="https://html.design/">Free Html Templates</a>
-    </p>
-</div>
-</footer>
-<!-- footer section -->
+  <!-- info section -->
+  <section class="info_section ">
+    <div class="container">
+      <div class="info_top">
+        <div class="info_logo">
+          <a href="">
+            <img src="images/logo.png" alt="">
+          </a>
+        </div>
+        <div class="info_form">
+          <form action="">
+            <input type="email" placeholder="Your email">
+            <button>
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="info_bottom layout_padding2">
+        <div class="row info_main_row">
+          <div class="col-md-6 col-lg-3">
+            <h5>
+              Address
+            </h5>
+            <div class="info_contact">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>
+                  Location
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call +01 1234567890
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope"></i>
+                <span>
+                  demo@gmail.com
+                </span>
+              </a>
+            </div>
+            <div class="social_box">
+              <a href="">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_links">
+              <h5>
+                Useful link
+              </h5>
+              <div class="info_links_menu">
+              <?php 
+                foreach ($navMenus as $navMenu) {
+                echo ' <ul class="navbar-nav  ">
+                    <li class="nav-item active">
+                    <a class="nav-link" href="index.php">' . $navMenu['btn1'] . '<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="about.php">' . $navMenu['btn2'] . '</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="treatment.php">' . $navMenu['btn3'] . '</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="doctor.php">' . $navMenu['btn4'] . '</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="testimonial.php">' . $navMenu['btn5'] . '</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="contact.php">' . $navMenu['btn6'] . '</a>
+                    </li>
+                </ul> ';
+                }
+            ?>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_post">
+              <h5>
+                LATEST POSTS
+              </h5>
+              <div class="post_box">
+                <div class="img-box">
+                  <img src="images/post1.jpg" alt="">
+                </div>
+                <p>
+                  Normal
+                  distribution
+                </p>
+              </div>
+              <div class="post_box">
+                <div class="img-box">
+                  <img src="images/post2.jpg" alt="">
+                </div>
+                <p>
+                  Normal
+                  distribution
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_post">
+              <h5>
+                News
+              </h5>
+              <div class="post_box">
+                <div class="img-box">
+                  <img src="images/post3.jpg" alt="">
+                </div>
+                <p>
+                  Normal
+                  distribution
+                </p>
+              </div>
+              <div class="post_box">
+                <div class="img-box">
+                  <img src="images/post4.png" alt="">
+                </div>
+                <p>
+                  Normal
+                  distribution
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end info_section -->
 
-<!-- jQery -->
-<script src="js/jquery-3.4.1.min.js"></script>
-<!-- bootstrap js -->
-<script src="js/bootstrap.js"></script>
-<!-- nice select -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
-<!-- owl slider -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<!-- datepicker -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
-<!-- custom js -->
-<script src="js/custom.js"></script>
+
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container">
+      <p>
+        &copy; <span id="displayYear"></span> All Rights Reserved By
+        <a href="https://html.design/">Free Html Templates</a>
+      </p>
+    </div>
+  </footer>
+  <!-- footer section -->
+
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- nice select -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/js/jquery.nice-select.min.js" integrity="sha256-Zr3vByTlMGQhvMfgkQ5BtWRSKBGa2QlspKYJnkjZTmo=" crossorigin="anonymous"></script>
+  <!-- owl slider -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <!-- datepicker -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
 
 
 </body>
